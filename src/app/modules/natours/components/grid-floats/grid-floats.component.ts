cb661f1829a90df09693d0eb8235c345ff0d54bd
alsaeedfayed <alsaeedfayed@gmail.com>
2024-03-27 12:09:00 +0200

    float grid

---
+++ src/app/modules/natours/components/grid-floats/grid-floats.component.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-floats',
+  templateUrl: './grid.component.html',
+  styleUrl: './grid-floats.component.css',
+})
+export class GridFloatsComponent { }
--- src/app/modules/natours/natours.module.ts
+++ src/app/modules/natours/natours.module.ts
@@ -3,17 +3,21 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { NatoursComponent } from './pages/natours/natours.component';
 import { NatoursRoutingModule } from './natours.routing.module';
+import { GridFloatsComponent } from "./components/grid-floats/grid-floats.component";
 
 
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    NatoursComponent
-  ],
-  imports: [
-    CommonModule,
-    NatoursRoutingModule
-  ]
+    declarations: [
+        HeaderComponent,
+        NatoursComponent,
+        GridFloatsComponent
+    ],
+    imports: [
+        CommonModule,
+        NatoursRoutingModule,
+
+
+    ]
 })
 export class NatoursModule { }
