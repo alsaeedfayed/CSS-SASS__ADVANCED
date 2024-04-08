d601ff20d18654634312038f38d885b88363e92a
alsaeedfayed <alsaeedfayed@gmail.com>
2024-04-08 15:53:25 +0200

    init-trillo

---
+++ src/app/modules/trillo-flexbox/trillo-flexbox.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TrilloFlexboxRoutingModule } from './trillo-flexbox-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TrilloFlexboxRoutingModule
+  ]
+})
+export class TrilloFlexboxModule { }
